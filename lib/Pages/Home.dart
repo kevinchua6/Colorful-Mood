32345ad8748691769158d17f9fba403ea83faad3
Zhang Ziqing <zhangziqing9926@gmail.com>
2021-09-04 18:26:26 +0800

    Add gradient to color grid

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -3,18 +3,31 @@ import '../MyHomePage.dart';
 import 'dart:math' as math;
 
 class Home extends StatelessWidget {
+  bool isTop(int num) {
+    return num < 18;
+  }
 
+  bool isLeft(int num) {
+    return num % 6 < 3;
+  }
 
+  Color? generateColor(int index) {
+    int thisColor = (isTop(index) ? 0 : 2) + (isLeft(index)? 0 : 1);
+    switch(thisColor) {
+      case 0:
+        return Colors.red[(3 - index % 6) * 100 + (4 - (index / 6).ceil()) * 100];
+      case 1:
+        return Colors.yellow[(index % 6 - 2)* 100 + (4 - (index / 6).ceil()) * 100];
+      case 2:
+        return Colors.blue[(2 - index % 6) * 100 + (index / 6).ceil() * 100];
+      case 3:
+        return Colors.green[(index % 6 - 4) * 100 + (index / 6).ceil() * 100];
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    const List<Color> colorss = [
-      Colors.red,
-      Colors.yellow,
-      Colors.blue,
-      Colors.green,
-
-    ];
 
     return GridView.count(
       // Create a grid with 2 columns. If you change the scrollDirection to
@@ -22,13 +35,14 @@ class Home extends StatelessWidget {
       crossAxisCount: 6,
       // Generate 100 widgets that display their index in the List.
       children: List.generate(36, (index) {
+
         return Container(
           width: 42,
           height: 42,
           padding: const EdgeInsets.all(3.0),
           child: DecoratedBox(
             decoration: BoxDecoration(
-              color: colorss[(index < 18 ? 0 : 2) + (index % 6 < 3 ? 0 : 1)]  ,
+              color: generateColor(index),
             ),
           ),
         );
